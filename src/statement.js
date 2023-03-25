57983e8a26f7091c7cafeebead076636f250b852
peter-huvenet <dhbhrl@gmail.com>
2023-03-25 23:52:14 +0900

    다형성 적용

--- src/statement.js
+++ src/statement.js
@@ -4,59 +4,72 @@ const formatToUsd = (num) => new Intl.NumberFormat("en-US",
         minimumFractionDigits: 2
     }).format(num / 100);
 
-const calculateAmount = (plays, performance) => {
-    const play = plays[performance.playID];
-    const audience = performance.audience;
-    switch (play.type) {
-        case "tragedy":
-            return 40000 + 1000 * Math.max(audience - 30, 0);
-        case "comedy":
-            return 30000 + 300 * audience + (audience > 20 ? 500 * audience : 0);
-        default:
-            throw new Error(`unknown type: ${play.type}`);
+class PerformanceCalculator {
+    constructor(audience) {
+        this.audience = audience;
     }
-};
-const calculateVolumeCredit = (plays, performance) => {
-    const play = plays[performance.playID];
-    const audience = performance.audience;
-    let volumeCredit = Math.max(audience - 30, 0);
-    switch (play.type) {
-        case "tragedy":
-            return volumeCredit;
-        case "comedy":
-            return volumeCredit + Math.floor(audience / 5);
-        default:
-            throw new Error(`unknown type: ${play.type}`);
-    }
-};
+}
 
-const calculateInvoice = (invoice, plays) => {
-    const result = {
-        customer: invoice.customer,
-        totalAmount: 0,
-        volumeCredits: 0,
-        performances: []
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        return 40000 + 1000 * Math.max(this.audience - 30, 0);
     };
+
+    get volumeCredit() {
+        return Math.max(this.audience - 30, 0);
+    };
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        return 30000 + 300 * this.audience + (this.audience > 20 ? 500 * this.audience : 0);
+    };
+
+    get volumeCredit() {
+        return Math.max(this.audience - 30, 0) + Math.floor(this.audience / 5);
+    };
+}
+
+const calculatorFactory = (performance, plays) => {
+    const type = plays[performance.playID].type;
+    switch (type) {
+        case "tragedy":
+            return new TragedyCalculator(performance.audience);
+        case "comedy":
+            return new ComedyCalculator(performance.audience);
+        default:
+            throw new Error(`unknown type: ${type}`);
+    }
+};
+const calculateInvoice = (invoice, plays) => {
+    const performances = [];
     invoice.performances.forEach(v => {
-        const performance = {...v};
-        performance.amount = calculateAmount(plays, performance);
-        result.performances.push(performance);
-        result.totalAmount += performance.amount;
-        result.volumeCredits += calculateVolumeCredit(plays, performance);
+        const calculator = calculatorFactory(v, plays);
+        performances.push({
+            amount: calculator.amount,
+            volumeCredit: calculator.volumeCredit,
+            name: plays[v.playID].name,
+            audience: v.audience
+        });
     });
-    return result;
+    return {
+        customer: invoice.customer,
+        performances: performances,
+        totalAmount: performances.reduce((total, v) => total + v.amount, 0),
+        volumeCredits: performances.reduce((total, v) => total + v.volumeCredit, 0)
+    };
 };
 
-const makeStatement = (invoice, plays) => {
+const makeStatement = (invoice) => {
     let result = `Statement for ${invoice.customer}\n`;
-    invoice.performances.forEach(v => result += ` ${plays[v.playID].name}: ${formatToUsd(v.amount)} (${v.audience} seats)\n`);
+    invoice.performances.forEach(v => result += ` ${v.name}: ${formatToUsd(v.amount)} (${v.audience} seats)\n`);
     result += `Amount owed is ${formatToUsd(invoice.totalAmount)}\n`;
     result += `You earned ${invoice.volumeCredits} credits\n`;
     return result;
 };
 
 const statement = (invoice, plays) => {
-    return makeStatement(calculateInvoice(invoice, plays), plays);
+    return makeStatement(calculateInvoice(invoice, plays));
 };
 
 module.exports = statement;
