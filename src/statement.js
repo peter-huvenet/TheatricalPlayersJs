755c0c6b4ea1869357e261c45b0495b9977ed2e4
peter-huvenet <dhbhrl@gmail.com>
2023-03-25 23:02:33 +0900

    다형성 적용 전 리팩토링

--- src/statement.js
+++ src/statement.js
@@ -1,43 +1,62 @@
+const formatToUsd = (num) => new Intl.NumberFormat("en-US",
+    {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+    }).format(num / 100);
 
-function statement (invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
-
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-        // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+const calculateAmount = (plays, performance) => {
+    const play = plays[performance.playID];
+    const audience = performance.audience;
+    switch (play.type) {
+        case "tragedy":
+            return 40000 + 1000 * Math.max(audience - 30, 0);
+        case "comedy":
+            return 30000 + 300 * audience + (audience > 20 ? 500 * audience : 0);
+        default:
+            throw new Error(`unknown type: ${play.type}`);
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+};
+const calculateVolumeCredit = (plays, performance) => {
+    const play = plays[performance.playID];
+    const audience = performance.audience;
+    let volumeCredit = Math.max(audience - 30, 0);
+    switch (play.type) {
+        case "tragedy":
+            return volumeCredit;
+        case "comedy":
+            return volumeCredit + Math.floor(audience / 5);
+        default:
+            throw new Error(`unknown type: ${play.type}`);
+    }
+};
+
+const calculateInvoice = (invoice, plays) => {
+    const result = {
+        customer: invoice.customer,
+        totalAmount: 0,
+        volumeCredits: 0,
+        performances: []
+    };
+    invoice.performances.forEach(v => {
+        const performance = {...v};
+        performance.amount = calculateAmount(plays, performance);
+        result.performances.push(performance);
+        result.totalAmount += performance.amount;
+        result.volumeCredits += calculateVolumeCredit(plays, performance);
+    });
     return result;
-}
+};
+
+const makeStatement = (invoice, plays) => {
+    let result = `Statement for ${invoice.customer}\n`;
+    invoice.performances.forEach(v => result += ` ${plays[v.playID].name}: ${formatToUsd(v.amount)} (${v.audience} seats)\n`);
+    result += `Amount owed is ${formatToUsd(invoice.totalAmount)}\n`;
+    result += `You earned ${invoice.volumeCredits} credits\n`;
+    return result;
+};
+
+const statement = (invoice, plays) => {
+    return makeStatement(calculateInvoice(invoice, plays), plays);
+};
 
 module.exports = statement;
